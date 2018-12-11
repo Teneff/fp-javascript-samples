f581e0544022cae26f09a97ac9f3556472758721
Miroslav Nedyalkov <miron@officernd.com>
2018-12-11 17:39:36 +0200

    Add sum

--- spec/jsArrayFunctionsSpec.js
+++ spec/jsArrayFunctionsSpec.js
@@ -12,7 +12,7 @@ describe('jsArrayFunctions', () => {
     });
 
     describe('sumIf', () => {
-        it('should return 20 for [1, 2, 3, 4, 5, 6, 7, 8, 9] and filter is isEven', () => {
+        it('should return 20 for [1, 2, 3, 4, 5, 6, 7, 8, 9] and predicate is isEven', () => {
             const isEven = n => n % 2 === 0;
             const result = sumIf([1, 2, 3, 4, 5, 6, 7, 8, 9], isEven);
 
--- src/higherOrderFunctions.js
+++ src/higherOrderFunctions.js
@@ -1,7 +1,12 @@
+function sum(array) {
+    const [first, ...rest] = array;
+    return array.length === 0 ? 0 : (first + sum(rest));
+}
+
 function sumIf(array, predicate) {
     const [first, ...rest] = array;
     const addValue = predicate(first) ? first : 0;
     return array.length === 0 ? 0 : addValue + sumIf(rest, predicate);
 }
 
-module.exports = { sumIf };
+module.exports = { sum, sumIf };
